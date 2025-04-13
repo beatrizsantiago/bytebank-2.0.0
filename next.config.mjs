d74528b00b95e36324ed680f707da22fb72c49bb
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-04-13 17:26:14 -0300

    ✨ Add headers configuration for caching static assets in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:all*\\.(svg|jpg|jpeg|png|webp|gif|ico)$',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, must-revalidate',
+          }
+        ],
+      },
+      {
+        source: '/fonts/:all*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      }
+      
+    ]
+  },
   env: {
     NEXT_PUBLIC_API_URL: 'http://localhost:3001/api',
   }
