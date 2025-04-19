6da44ed043b1f53c565ca71706dafe4eefacb746
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-04-19 12:24:02 -0300

    🎨 Refactor transaction types to streamline definitions and improve code organization

--- src/types/transaction.ts
+++ src/types/transaction.ts
@@ -1,23 +1,14 @@
+import Transaction from '@domain/entities/Transaction';
+
 export type KindType = 'DEPOSIT' | 'CURRENCY_EXCHANGE' | 'DOC_TED' | 'LEASING';
-
-export type AddTransactionParams = {
-  kind: KindType;
-  value: number;
-};
-
-export type UpdateTransactionParams = {
-  kind: KindType;
-  value: number;
-};
-
-export type TransactionType = {
-  _id: string;
-  kind: KindType;
-  value: number;
-  date: string;
-};
 
 export type TransactionOptionType = {
   label: string;
   value: string;
+};
+
+export type TransactionList = {
+  data: Transaction[];
+  totalPages: number;
+  currentPage: number;
 };
